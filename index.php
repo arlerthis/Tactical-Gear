e4ff95ebad77019b4b22c0278313e0a322da2718
arlerthis <154047873+arlerthis@users.noreply.github.com>
2023-12-17 15:00:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,239 @@
+
+
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <title>Tactical Equipment</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <script>
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- //Meta tag Keywords -->
+    <!-- Custom-Files -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <!-- Style-CSS -->
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome-icons -->
+    <!-- /Fonts -->
+    <link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+    <!-- //Fonts -->
+
+</head>
+
+<body>
+    <div class="main-sec">
+        <!-- //header -->
+        <header class="py-sm-3 pt-3 pb-2" id="home">
+            <div class="container">
+                <!-- nav -->
+                <div class="top-w3pvt d-flex">
+                    <div id="logo">
+                        <h1> <a href="index.php"><span class="log-w3pvt">TACTICAL</span> GEAR</a> <label class="sub-des">Online Store</label></h1>
+                    </div>
+
+                    <div class="forms ml-auto">
+                        <a href="login.php" class="btn"><span class="fa fa-user-circle-o"></span> Sign In</a>
+                        <!-- <a href="register.php" class="btn"><span class="fa fa-pencil-square-o"></span> Sign Up</a> -->
+                    </div>
+                </div>
+                <div class="nav-top-wthree">
+                    <nav>
+                        <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+                        <input type="checkbox" id="drop" />
+                        <ul class="menu">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About Us</a></li>
+
+                            <li><a href="shop.php">Collections</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <!-- //nav -->
+                    <div class="search-form ml-auto">
+                        <div class="form-w3layouts-grid">
+                            </form>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </header>
+        <!-- //header -->
+        <!--/banner-->
+        <div class="banner-wthree-info container">
+            <div class="row">
+                <div class="col-lg-5 banner-left-info">
+                    <h3>Tactical Equipment<span> by HH</span></h3>
+                    <a href="shop.php" class="btn shop">Shop Now</a>
+                </div>
+
+                <div class="col-lg-7 banner-img">
+                    <img src="images/bg1.png" alt="part image" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- //banner-->
+    <!--/banner-bottom -->
+    <section class="banner-bottom py-5">
+        <div class="container py-md-3">
+            <div class="row grids-wthree-info text-center">
+                <div class="col-lg-4 ab-content">
+                    <div class="ab-info-con">
+                        <h4>Pakaian</h4>
+                        <p>Tactical Equipment Shop menyediakan Baju atau Celana yang berkualitas menggunakan bahan Ripstok.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 ab-content">
+                    <div class="ab-info-con">
+                        <h4>Alat Camping</h4>
+                        <p>Tactical Equipment Shop menyediakan kebutuhan alat Camping seperti tenda, matras, flysheet, dll.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 ab-content">
+                    <div class="ab-info-con">
+                        <h4>Survival Kit</h4>
+                        <p>Tactical Equipment Shop menyediakan alat kit survival seperti pisau lipat, kompor portable, gas portable, senter, dll.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!--/collections -->
+    <section class="banner-bottom py-5">
+        <div class="container py-md-5">
+
+            <h3 class="title-wthree mb-lg-5 mb-4 text-center">Make Your Style Is Cool !</h3>
+            <div class="row text-center">
+
+
+                <div class="col-md-4 content-gd-wthree">
+                    <img src="images/cream.webp" class="img-fluid" alt="" />
+                </div>
+                <div class="col-md-4 content-gd-wthree ab-content py-lg-5 my-lg-5">
+                    <h4>GIFTS FOR THE NEXT ADVENTURE</h4>
+                    <h6>We've got the perfect gifts to get geared up for the next mission.</h6>
+                    <a href="shop.php" class="btn shop mt-3">Shop Now</a>
+
+                </div>
+                <div class="col-md-4 content-gd-wthree">
+                    <img src="images/abu.webp" class="img-fluid" alt="" />
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!--/gallery -->
+    <section class="banner-bottom py-5">
+        <div class="container py-md-5">
+
+
+            <div class="row">
+                <div class="col-lg-4 gallery-content-info text-center mt-lg-5">
+                    <h3 class="title-wthree mb-lg-5 mb-4">SURVIVAL KIT </h3>
+                    <p>Lengkapi peralatan survival mu di Tactical Equipment Shop sekarang juga!</p>
+                    <a href="shop.php" class="btn shop mt-3">Shop Now</a>
+
+                </div>
+                <div class="col-lg-8 gallery-content">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6 gal-img">
+                            <a href="#gal1"><img src="images/kompor bunga.jpeg" alt="Baggage" class="img-fluid mt-4"></a>
+                        </div>
+                        <div class="col-md-4 col-sm-6 gal-img">
+                            <a href="#gal2"><img src="images/kompor kotak.jpg" alt="Baggage" class="img-fluid mt-4"></a>
+                        </div>
+                        <div class="col-md-4 col-sm-6 gal-img">
+                            <a href="#gal3"><img src="images/kompor segitiga.jpg" alt="Baggage" class="img-fluid mt-4"></a>
+                        </div>
+                        <div class="col-md-4 col-sm-6 gal-img">
+                            <a href="#gal1"><img src="images/gas.jpg" alt="Baggage" class="img-fluid mt-4"></a>
+                        </div>
+                        <div class="col-md-4 col-sm-6 gal-img">
+                            <a href="#gal2"><img src="images/senter.jpg" alt="Baggage" class="img-fluid mt-4"></a>
+                        </div>
+                        <div class="col-md-4 col-sm-6 gal-img">
+                            <a href="#gal3"><img src="images/pisau lipat.jpg" alt="Baggage" class="img-fluid mt-4"></a>
+                        </div>
+
+                    </div>
+                    <!-- gallery popups -->
+                    <!-- popup-->
+                    <div id="gal1" class="popup-effect">
+                        <div class="popup">
+                            <img src="images/g1.jpg" alt="Popup image" class="img-fluid mt-4" />
+                            <a class="close" href="#gallery">&times;</a>
+                        </div>
+                    </div>
+                    <!-- //popup -->
+                    <!-- popup-->
+                    <div id="gal2" class="popup-effect">
+                        <div class="popup">
+                            <img src="images/g2.jpg" alt="Popup image" class="img-fluid mt-4" />
+                            <a class="close" href="#gallery">&times;</a>
+                        </div>
+                    </div>
+                    <!-- //popup -->
+                    <!-- popup-->
+                    <div id="gal3" class="popup-effect">
+                        <div class="popup">
+                            <img src="images/g3.jpg" alt="Popup image" class="img-fluid mt-4" />
+                            <a class="close" href="#gallery">&times;</a>
+                        </div>
+                    </div>
+                    <!-- //popup -->
+                    <!-- //gallery popups -->
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- //gallery-->
+
+            <div class="w3ls-fsocial-grid">
+                <h3 class="sub-w3ls-headf">Follow Us</h3>
+                <div class="social-ficons">
+                    <ul>
+                        <li><a href="#"><span class="fa fa-facebook"></span> Facebook</a></li>
+                        <li><a href="#"><span class="fa fa-twitter"></span> Twitter</a></li>
+                        <li><a href="#"><span class="fa fa-google"></span>Google</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="move-top text-center mt-lg-4 mt-3">
+                <a href="#home"><span class="fa fa-angle-double-up" aria-hidden="true"></span></a>
+            </div>
+        </div>
+    </div>
+    <!-- //footer -->
+    
+
+    <!-- copyright -->
+    <div class="cpy-right text-center py-3">
+        <p>© artbykhotim
+        </p>
+
+    </div>
+    <!-- //copyright -->
+
+</body>
+
+</html>
+   
